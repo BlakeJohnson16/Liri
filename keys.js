a254e46b29db5b2036f5dd0dcb9d46cfd6cc7f55
BlakeJohnson16 <bman0511@yahoo.com>
2017-09-28 17:16:30 -0400

    homework 10!

---
+++ keys.js
@@ -0,0 +1,6 @@
+exports.twitterKeys = {
+		consumer_key: "ofWjPcatcijDwrnlW7qvPJpqL",
+		consumer_secret: "bfqV0N4PHV0QUr0OYpKwyrrUmpRVpEm3ZnIOvgqIaVLnagqnSn",
+		access_token_key: "960398946-iakZ7nfZMagFibNjEkQqp3ExaJm4gcIVJ1TEuHnE",
+		access_token_secret: "iQaGBWFstBPWTADpmDrYoOeJBXiVwERAIm3QisYu6tRrZ"
+  };
